cd0a4aedebc1dc42bd383e551b45d44e63e0ddfe
geordydecena <geordydecena@gmail.com>
2023-11-08 15:22:54 -0500

    fixing minor typos

--- packages/front-end/src/components/userCount.tsx
+++ packages/front-end/src/components/userCount.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const UserCount = ({ userCount }) => {
     return (
-        <p className="p-2 ml-0.5 h-8 w-40 flex items-center leading-4">{`${userCount} result${userCount > 1 && "s"}`}</p>
+        <p className="p-2 ml-0.5 h-8 w-40 flex items-center leading-4">{`${userCount} result${userCount !== 1 ? "s" : ""}`}</p>
     );
 };
 
